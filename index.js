a89f2e42fcecafee1aa109baca47bbb9e035c5eb
harisheoran <harisheoran01@gmail.com>
2023-11-16 18:03:04 +0530

    added bind mount, dockerignore, actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("<h1> Full end to end DevOps Project</h1>")
+    res.send("<h1> Hello there, Full end to end DevOps project. (Node JS)</h1>")
 })
 
 const port = process.env.PORT || 3000
